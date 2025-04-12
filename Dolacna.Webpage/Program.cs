1c77f4657aa010bbc0daa1a272774537ddab53f0
Juraj <j.paska@cid.com>
2025-04-12 15:07:31 +0200

    fix: allow cors

--- Dolacna.Webpage/Program.cs
+++ Dolacna.Webpage/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 app.UseStaticFiles();
 app.Run();
